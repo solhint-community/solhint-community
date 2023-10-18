c91167855b9a8502aeb7cb34a95852379827e247
capu <hi@capu.tech>
2023-10-18 12:58:44 -0300

    test: dont report on builtin variadic functions

--- test/rules/naming/named-parameters-function.js
+++ test/rules/naming/named-parameters-function.js
@@ -1,6 +1,6 @@
 const linter = require('../../../lib/index')
 const { assertNoWarnings, assertWarnsCount, assertErrorMessage } = require('../../common/asserts')
-const { contractWith, multiLine } = require('../../common/contract-builder')
+const { contractWith, multiLine, funcWith } = require('../../common/contract-builder')
 
 describe('Linter - named-parameters-function', () => {
   describe('GIVEN default rule settings', function () {
@@ -36,6 +36,52 @@ describe('Linter - named-parameters-function', () => {
         )
       )
       assertNoWarnings(linter.processStr(code, config))
+    })
+  })
+
+  describe('GIVEN a setting of one maximum positional argument', function () {
+    const config = {
+      rules: { 'named-parameters-function': ['warn', 1] },
+    }
+    describe('builitin functions with named parameters', function () {
+      it('WHEN calling require with a positional error message THEN it reports', function () {
+        const code = funcWith('require(false, "this is an error");')
+        assertWarnsCount(linter.processStr(code, config), 1)
+      })
+      it('WHEN calling require with a named error message THEN it does NOT report', function () {
+        const code = funcWith('require({condition: false, message: "this is an error"});')
+        assertNoWarnings(linter.processStr(code, config))
+      })
+      it('WHEN calling require without an error message THEN it does NOT report', function () {
+        const code = funcWith('require(false);')
+        assertNoWarnings(linter.processStr(code, config))
+      })
+
+      it('WHEN calling builtin function mulmod with named parameters THEN it does NOT report', function () {
+        const code = funcWith('uint r = mulmod({x: 100, y: 7, k: 4});')
+        assertNoWarnings(linter.processStr(code, config))
+      })
+      it('WHEN calling builtin function ecrecover with named parameters THEN it does NOT report', function () {
+        const code = funcWith('uint r = ecrecover({hash: foo, v: bar, r: baz, s: moo});')
+        assertNoWarnings(linter.processStr(code, config))
+      })
+      it('WHEN calling builtin function ecrecover with positional parameters THEN it reports', function () {
+        const code = funcWith('uint r = ecrecover(foo, bar, baz, moo);')
+        assertWarnsCount(linter.processStr(code, config), 1)
+      })
+    })
+
+    describe('builtin variadic functions', function () {
+      it('WHEN calling abi.decode with positional arguments, THEN it does NOT report', function () {
+        const code = funcWith('(uint a, uint b, uint c) = abi.decode(data, (uint, uint, uint));')
+        assertNoWarnings(linter.processStr(code, config))
+      })
+      ;['encode', 'encodePacked', 'encodeWithSelector', 'encodeWithSignature'].forEach((method) => {
+        it(`WHEN calling abi.${method} with positional arguments, THEN it does NOT report`, function () {
+          const code = funcWith(`bytes foo = abi.${method}(foo, bar, baz);`)
+          assertNoWarnings(linter.processStr(code, config))
+        })
+      })
     })
   })
 
