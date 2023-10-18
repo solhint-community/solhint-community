daf57ec1f827037f0d1e5f78ba39792e0a8fd38f
capu <hi@capu.tech>
2023-10-18 10:37:30 -0300

    refactor named-parameters-function tests to be more gherkin-y

--- test/rules/naming/named-parameters-function.js
+++ test/rules/naming/named-parameters-function.js
@@ -3,7 +3,57 @@ const { assertNoWarnings, assertWarnsCount, assertErrorMessage } = require('../.
 const { contractWith, multiLine } = require('../../common/contract-builder')
 
 describe('Linter - named-parameters-function', () => {
-  it('GIVEN a setting of 4, should NOT warn on calling with four positional arguments', () => {
+  describe('GIVEN default rule settings', function () {
+    const config = {
+      rules: { 'named-parameters-function': 'warn' },
+    }
+
+    it('WHEN calling with two positional arguments THEN it does NOT report', () => {
+      const code = contractWith(
+        multiLine('function foo(uint a, uint b) {}', `function bar (){foo(1,2);}`)
+      )
+      assertNoWarnings(linter.processStr(code, config))
+    })
+    it('WHEN calling with four positional arguments THEN it reports', () => {
+      const code = contractWith(
+        multiLine(
+          'function foo(uint a, uint b, uint c, uint d) {}',
+          `function bar (){foo(1,2,3,4);}`
+        )
+      )
+      const report = linter.processStr(code, config)
+      assertWarnsCount(report, 1)
+      assertErrorMessage(
+        report,
+        'Call to function with arity > 3 is using positional arguments. Use named arguments instead.'
+      )
+    })
+    it('WHEN calling with four named arguments THEN it does NOT report', () => {
+      const code = contractWith(
+        multiLine(
+          'function foo(uint a, uint b, uint c, uint d) {}',
+          `function bar (){foo({a: 1, b: 2, c:3, d:4});}`
+        )
+      )
+      assertNoWarnings(linter.processStr(code, config))
+    })
+  })
+
+  describe('GIVEN a setting of zero', function () {
+    const config = {
+      rules: { 'named-parameters-function': ['warn', 0] },
+    }
+    it('WHEN calling no arguments THEN it does NOT report', () => {
+      const code = contractWith(multiLine('function foo() {}', `function bar (){foo();}`))
+      assertNoWarnings(linter.processStr(code, config))
+    })
+    it('WHEN calling with one positional argument THEN it reports', () => {
+      const code = contractWith(multiLine('function foo(uint a) {}', `function bar (){foo(3);}`))
+      assertWarnsCount(linter.processStr(code, config), 1)
+    })
+  })
+
+  it('GIVEN a setting of 4, THEN it does NOT warn on calls with four positional arguments', () => {
     const code = contractWith(
       multiLine('function foo(uint a, uint b, uint c, uint d) {}', `function bar (){foo(1,2,3,4);}`)
     )
@@ -15,7 +65,7 @@ describe('Linter - named-parameters-function', () => {
     assertNoWarnings(report)
   })
 
-  it('GIVEN a default solhint config, THEN the rule is disabled and no errors are reported', () => {
+  it('GIVEN a recommended solhint config, THEN the rule is disabled and no errors are reported', () => {
     const code = contractWith(
       multiLine(
         'function foo(uint a, uint b, uint c, uint d) public {}',
@@ -31,27 +81,7 @@ describe('Linter - named-parameters-function', () => {
     assertNoWarnings(report)
   })
 
-  it('GIVEN a setting of 0, should NOT warn on calling with no arguments', () => {
-    const code = contractWith(multiLine('function foo() {}', `function bar (){foo();}`))
-
-    const report = linter.processStr(code, {
-      rules: { 'named-parameters-function': ['warn', 0] },
-    })
-
-    assertNoWarnings(report)
-  })
-
-  it('GIVEN a setting of 0, should warn on calls with one positional argument', () => {
-    const code = contractWith(multiLine('function foo(uint a) {}', `function bar (){foo(3);}`))
-
-    const report = linter.processStr(code, {
-      rules: { 'named-parameters-function': ['warn', 0] },
-    })
-
-    assertWarnsCount(report, 1)
-  })
-
-  it('GIVEN a setting of 2, should warn on calls with 3 positional argument', () => {
+  it('GIVEN a setting of 2, THEN it warns on calls with 3 positional argument', () => {
     const code = contractWith(
       multiLine('function foo(uint a, uint b, uint c) {}', `function bar (){foo(1,2,3);}`)
     )
@@ -65,47 +95,5 @@ describe('Linter - named-parameters-function', () => {
       report,
       'Call to function with arity > 2 is using positional arguments. Use named arguments instead.'
     )
-  })
-
-  it('GIVEN default settings, should NOT warn on calling with two positional arguments', () => {
-    const code = contractWith(
-      multiLine('function foo(uint a, uint b) {}', `function bar (){foo(1,2);}`)
-    )
-
-    const report = linter.processStr(code, {
-      rules: { 'named-parameters-function': 'warn' },
-    })
-
-    assertNoWarnings(report)
-  })
-
-  it('GIVEN default settings, should warn on calling with four positional arguments', () => {
-    const code = contractWith(
-      multiLine('function foo(uint a, uint b, uint c, uint d) {}', `function bar (){foo(1,2,3,4);}`)
-    )
-
-    const report = linter.processStr(code, {
-      rules: { 'named-parameters-function': 'warn' },
-    })
-
-    assertWarnsCount(report, 1)
-    assertErrorMessage(
-      report,
-      'Call to function with arity > 3 is using positional arguments. Use named arguments instead.'
-    )
-  })
-
-  it('GIVEN default settings, should NOT warn on calling with four named arguments', () => {
-    const code = contractWith(
-      multiLine(
-        'function foo(uint a, uint b, uint c, uint d) {}',
-        `function bar (){foo({a: 1, b: 2, c:3, d:4});}`
-      )
-    )
-
-    const report = linter.processStr(code, {
-      rules: { 'named-parameters-function': 'warn' },
-    })
-    assertNoWarnings(report)
   })
 })
