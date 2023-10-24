919468a790fa3ee8e7fac4eef64858ee645e7aa8
capu <hi@capu.tech>
2023-10-24 20:14:34 -0300

    tests for check-send-result
    - consider passing the return value to any function a valid usage
    - test cases where the function call is not the immediate parent of the
      member access, such as when passing a custom gas amount, where the
      hierarchy is, for example in x.send{gas: 9000}(1)
    
    FunctionCall
    |- NameValueExpression
    | |- NameValueList
    | | |- gas
    | | |- 9000
    | |- MemberAccess
    | | |- x
    | | |- send
    |- 1

--- test/rules/security/check-send-result.js
+++ test/rules/security/check-send-result.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 const linter = require('../../../lib/index')
-const funcWith = require('../../common/contract-builder').funcWith
+const { funcWith, multiLine } = require('../../common/contract-builder')
 
 describe('Linter - check-send-result', () => {
   it('should return "send" call verification error', () => {
@@ -26,21 +26,48 @@ describe('Linter - check-send-result', () => {
 
   it('do not emit error when a require is used', () => {
     const code = funcWith('require(x.send(1));')
-
     const report = linter.processStr(code, {
       rules: { 'check-send-result': 'error' },
     })
+    assert.equal(report.errorCount, 0)
+  })
 
+  it('do not emit error when a require is used with a custom gas amount', () => {
+    const code = funcWith('require(x.send{gas: 9000}(1));')
+    const report = linter.processStr(code, {
+      rules: { 'check-send-result': 'error' },
+    })
     assert.equal(report.errorCount, 0)
   })
 
   it('do not emit error when a require is used upper in the tree', () => {
     const code = funcWith('require(!x.send(1));')
-
     const report = linter.processStr(code, {
       rules: { 'check-send-result': 'error' },
     })
+    assert.equal(report.errorCount, 0)
+  })
 
+  it('do not emit error when return value is assigned to a new variable', () => {
+    let report = linter.processStr(funcWith('bool success = x.send(1);'), {
+      rules: { 'check-send-result': 'error' },
+    })
+    assert.equal(report.errorCount, 0)
+    report = linter.processStr(funcWith('bool success = x.send{gas: 4000}(1);'), {
+      rules: { 'check-send-result': 'error' },
+    })
+    assert.equal(report.errorCount, 0)
+  })
+
+  // no-unused-var should take it from there
+  it('do not emit error when return value is assigned to an existing variable', () => {
+    let report = linter.processStr(funcWith('bool success = false; success = x.send(1);'), {
+      rules: { 'check-send-result': 'error' },
+    })
+    assert.equal(report.errorCount, 0)
+    report = linter.processStr(funcWith('bool success;', 'success = x.send(1);'), {
+      rules: { 'check-send-result': 'error' },
+    })
     assert.equal(report.errorCount, 0)
   })
 
@@ -64,23 +91,51 @@ describe('Linter - check-send-result', () => {
     assert.equal(report.errorCount, 0)
   })
 
-  it('emit error when an arbitrary function surrounds the expression', () => {
-    const code = funcWith('f(x.send(1));')
-
+  it('do not emit error when the expression is passed to a function', () => {
+    const code = funcWith('doThingWithSendResult(x.send(1));')
     const report = linter.processStr(code, {
       rules: { 'check-send-result': 'error' },
     })
+    assert.equal(report.errorCount, 0)
+  })
 
+  it('do not crash on other .send member access without a parent FunctionCall', function () {
+    const code = funcWith(multiLine('uint256 amount = foo.send;', 'recipient.send(amount);'))
+    const report = linter.processStr(code, {
+      rules: { 'check-send-result': 'error' },
+    })
     assert.equal(report.errorCount, 1)
+  })
+
+  it('do not emit error when the expression is passed to a function using a custom gas amount', function () {
+    const code = funcWith('doThingWithSendResult(x.send{gas: 4000}(1));')
+    const report = linter.processStr(code, {
+      rules: { 'check-send-result': 'error' },
+    })
+    assert.equal(report.errorCount, 0)
   })
 
   it('should not emit error when the send() is used for an ERC777', () => {
     const code = funcWith('erc777.send(recipient, amount, "");')
-
     const report = linter.processStr(code, {
       rules: { 'check-send-result': 'error' },
     })
+    assert.equal(report.errorCount, 0)
+  })
 
+  it('should not emit error when the send() is called on an ERC777', () => {
+    const code = funcWith('erc777.send(recipient, amount, "");')
+    const report = linter.processStr(code, {
+      rules: { 'check-send-result': 'error' },
+    })
+    assert.equal(report.errorCount, 0)
+  })
+
+  it('should not emit error when the send() is used for an ERC777 with a custom gas amount', () => {
+    const code = funcWith('erc777.send{gas: 15000}(recipient, amount, "");')
+    const report = linter.processStr(code, {
+      rules: { 'check-send-result': 'error' },
+    })
     assert.equal(report.errorCount, 0)
   })
 })
