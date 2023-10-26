548073efca217eb45e3639672a792320886eaaed
danilo neves cruz <cruzdanilo@gmail.com>
2023-10-26 12:29:01 -0300

    ✅ one-contract-per-file: test libraries and interfaces

--- test/rules/best-practises/one-contract-per-file.js
+++ test/rules/best-practises/one-contract-per-file.js
@@ -29,6 +29,26 @@ describe('Linter - one-contract-per-file', () => {
     assertErrorMessage(report, 'Found more than one contract per file. 2 contracts found!')
   })
 
+  it('should not raise error for ONE contract and multiple interfaces in the same file', () => {
+    const code = contracts.ONE_CONTRACT_WITH_INTERFACES
+
+    const report = linter.processStr(code, {
+      rules: { 'one-contract-per-file': 'error' },
+    })
+
+    assertNoWarnings(report)
+  })
+
+  it('should not raise error for ONE library and multiple interfaces in the same file', () => {
+    const code = contracts.ONE_LIBRARY_WITH_INTERFACES
+
+    const report = linter.processStr(code, {
+      rules: { 'one-contract-per-file': 'error' },
+    })
+
+    assertNoWarnings(report)
+  })
+
   it('should raise error for TWO contracts in same file', () => {
     const code = contracts.TWO_CONTRACTS
     const report = linter.processStr(code, {
@@ -47,5 +67,16 @@ describe('Linter - one-contract-per-file', () => {
 
     assertErrorCount(report, 1)
     assertErrorMessage(report, 'Found more than one contract per file. 3 contracts found!')
+  })
+
+  it('should raise error for TWO libraries in same file', () => {
+    const code = contracts.TWO_LIBRARIES
+
+    const report = linter.processStr(code, {
+      rules: { 'one-contract-per-file': 'error' },
+    })
+
+    assertErrorCount(report, 1)
+    assertErrorMessage(report, 'Found more than One contract per file. 2 contracts found!')
   })
 })
