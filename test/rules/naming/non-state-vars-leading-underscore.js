817c44aaf1388ff761b282d639484b1655519028
capu <hi@capu.tech>
2024-01-11 16:36:06 -0300

    comprehensive testcases for non-state-vars-leading-underscore

--- test/rules/naming/non-state-vars-leading-underscore.js
+++ test/rules/naming/non-state-vars-leading-underscore.js
@@ -7,17 +7,70 @@ const config = {
 }
 
 describe('non-state-vars-leading-underscore', () => {
-  it('should raise an error if a block variable does not start with an underscore', () => {
-    const code = contractWith('function foo() public { uint myVar; }')
-    const report = processStr(code, config)
-    assert.equal(report.errorCount, 1)
-    assert.ok(report.messages[0].message === `'myVar' should start with _`)
+  describe('non state vars should have a leading underscore', function () {
+    ;[
+      {
+        description: 'block variable',
+        underscore: contractWith('function foo() public { uint _myVar; }'),
+        noUnderscore: contractWith('function foo() public { uint myVar; }'),
+      },
+      {
+        description: 'return variable',
+        underscore: contractWith('function foo() public returns (uint256 _foo){}'),
+        noUnderscore: contractWith('function foo() public returns (uint256 foo){}'),
+      },
+      {
+        description: 'function param',
+        underscore: contractWith('function foo( uint256 _foo ) public {}'),
+        noUnderscore: contractWith('function foo( uint256 foo ) public {}'),
+      },
+      {
+        description: 'file-level constant',
+        underscore: 'uint256 constant _IMPORTANT_VALUE = 420;',
+        noUnderscore: 'uint256 constant IMPORTANT_VALUE = 420;',
+      },
+    ].forEach(({ description, underscore, noUnderscore }) => {
+      it(`should raise an error if a ${description} does not start with an underscore`, () => {
+        const report = processStr(noUnderscore, config)
+        assert.equal(report.errorCount, 1)
+        assert(report.messages[0].message.includes('should start with _'))
+      })
+
+      it(`should NOT raise an error if a ${description} starts with an underscore`, () => {
+        const report = processStr(underscore, config)
+        assert.equal(report.errorCount, 0)
+      })
+    })
   })
 
-  it('should not raise an error if a block variable starts with an underscore', () => {
-    const code = contractWith('function foo() public { uint _myVar; }')
-    const report = processStr(code, config)
+  describe('state vars should NOT have a leading underscore', function () {
+    ;[
+      {
+        description: 'immutable state variable',
+        noUnderscore: contractWith('uint256 immutable foo;'),
+        withUnderscore: contractWith('uint256 immutable _foo;'),
+      },
+      {
+        description: 'constant state "variable"',
+        noUnderscore: contractWith('uint256 constant foo;'),
+        withUnderscore: contractWith('uint256 constant _foo;'),
+      },
+      {
+        description: 'mutable state variable',
+        noUnderscore: contractWith('uint256 foo;'),
+        withUnderscore: contractWith('uint256 _foo;'),
+      },
+    ].forEach(({ description, noUnderscore, withUnderscore }) => {
+      it(`should raise an error if a ${description} starts with an underscore`, () => {
+        const report = processStr(withUnderscore, config)
+        assert.equal(report.errorCount, 1)
+        assert(report.messages[0].message.includes('should not start with _'))
+      })
 
-    assert.equal(report.errorCount, 0)
+      it(`should NOT raise an error if a ${description} doesnt start with an underscore`, () => {
+        const report = processStr(noUnderscore, config)
+        assert.equal(report.errorCount, 0)
+      })
+    })
   })
 })
